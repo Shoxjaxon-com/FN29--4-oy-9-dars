1be0746d8145a93dae04db96d472d5f5db6bff87
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-10-08 05:06:07 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,169 @@
+// === Oddiy masala === //
+
+// 1-masala
+
+// let arr = [1,2,3]
+// console.log(arr[0]);
+// console.log(arr[2]);
+
+// 2-masala
+
+// let arr =[1,2,3,4,5]
+// arr[4] = 'Salom'
+// console.log(arr); 
+
+// 3-masala
+
+// let arr = [];
+// arr.unshift('Olma')
+// arr.unshift('Behi')
+// arr.unshift('Anor')
+// console.log(arr);
+
+// 4-masala
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// console.log(arr.length);
+
+// 5-masala
+
+// let arr = [1,2,4,5,6]
+// arr.unshift(3)
+// console.log(arr);
+
+// 6-masala
+// let arr = [1,2,4,5,6]
+// arr.pop()
+// console.log(arr);
+
+// 7-masala
+
+// let arr = [1,2,4,5,6]
+// arr.unshift('Anor')
+// console.log(arr);
+
+// 8-masala
+// let arr = [1,2,4,5,6]
+// arr.shift()
+// console.log(arr);
+
+// 9-masala
+
+// let arr = [1,2,3,4,5,6]
+// for(let i = 0; i<= arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 10-masala
+
+// let arr = [1,2,3,4,5,6]
+
+// let qiymat = 5;
+// if(arr.includes(qiymat)){
+//     console.log('Mavjud ');
+// }else{
+//     console.log('Mavjud emas');
+// }
+
+
+// === For === //
+// 1-masala
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i = 0; i<= arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 2-masala
+
+// let arr = ['Olma','Gilos','Behi','Anjir','Madarin','Ananas'];
+// for(let i = 0; i< arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 3-masala
+
+// let arr = [1,2,4,5,6]
+// for(let i = 0; i <arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 4-masala
+
+// let arr = [1,2,4,5,6]
+// for(let i = 0; i <arr.length; i++){
+//     if(arr[i] % 2 == 1){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 5 -masala
+// let arr = [1, 2, -3, 4, -5, -6]
+
+// for(let i=0; i < arr.length; i++){
+//     if (arr[i] > 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+// 6-masla
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let res=arr.reverse()
+// console.log(res);
+
+
+// 7 -masala
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let sum=0;
+// for(let i =0; i< arr.length; i++){
+//     sum+=arr[i]
+// }
+// console.log(sum);
+// 8-masala 
+
+// let arr = [1,2,3,5,6];
+// let kopaytma =[];
+// for(let i = 0; i <arr.length; i++){
+//   kopaytma.push(arr[i]*2)
+// }
+// console.log(kopaytma);
+
+// 9-masala
+// let arr = [1,2,3,4,5,6]
+// let res = Math.max(...arr)
+// console.log(res);
+
+// 10-masala
+// let arr = [1,2,3,4,5,6]
+// let res = Math.min(...arr)
+// console.log(res);
+
+// 11-masala
+
+// let arr = [-1,2,-3,-4,5,-6]
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 12-masala
+// let arr =[1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5]
+// let sum =0;
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i] > 0){
+//         sum+=arr[i]
+//     }
+// }
+// console.log(sum);
+
+// 15-masala
+
+let arr =[1,2,3,4,5,6,7,8,9,10]
+for(let i =1; i< arr.length;i++){
+    console.log(arr[i]);
+}
